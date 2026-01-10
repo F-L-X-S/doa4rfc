14393da691c518a948a19052e93fb4af81bb68e7
F-L-X-S <felixschuelke@icloud.com>
2026-01-10 20:04:43 +0100

    Added traits for flexframesync

--- include/multisync/synctraits.h
+++ include/multisync/synctraits.h
@@ -6,7 +6,7 @@
  * 
  * The MultiSync class utilizes the abstract SyncTraits struct that defines the Liquid-DSP interface for 
  * the specified synchronizer type. SyncTraits defines the C-function call of the Liquid-DSP synchronizer 
- * corresponding to the C++ functions used in MultiSync (Create, Reset, Execute, Destroy, GetCfr).
+ * corresponding to the C++ functions used in MultiSync (Create, Reset, Execute, Destroy, GetFrameSamps).
  * 
  * Enabling MultiSync for different synchronizer types is achieved by specializing the SyncTraits-template for the required synchronizer type. 
  * 
@@ -43,14 +43,14 @@ concept SyncTraitsConcept = requires(
 
     std::complex<float>* x,                 // Pointer to input samples array for Execute function
     unsigned int n,                         // Number of input samples to read for Execute function
-    std::vector<std::complex<float>>* X     // Vector to store CFR for GetCfr function
+    std::vector<std::complex<float>>* X     // Vector to store samples for GetFrameSamps function
 ) {
     // Check function signatures
     { SyncTraitsSpecification::Create(createParams, callback, nullptr) } -> std::same_as<typename SyncTraitsSpecification::SynchronizerType>;
     { SyncTraitsSpecification::Reset(synchronizer) } -> std::same_as<void>;
     { SyncTraitsSpecification::Execute(synchronizer, x, n) } -> std::same_as<int>;
     { SyncTraitsSpecification::Destroy(synchronizer) } -> std::same_as<void>;
-    { SyncTraitsSpecification::GetCfr(synchronizer, X) } -> std::same_as<void>;
+    { SyncTraitsSpecification::GetFrameSamps(synchronizer, X) } -> std::same_as<void>;
 };
 
 /**
@@ -67,9 +67,10 @@ struct SyncTraits {
 // -------------------- Specializations for different synchronizer types --------------------
 // ------------------------------------------------------------------------------------------
 
+// -------------------- OFDM Frame Synchronizer --------------------
 /**
  * @brief SyncTraits specialization for the OFDM frame synchronizer.
- * Define the functions (Create, Reset, Execute, Destroy, GetCfr) for the OFDM frame synchronizer
+ * Define the functions (Create, Reset, Execute, Destroy, GetFrameSamps) for the OFDM frame synchronizer
  * 
  * @tparam  Synchronizer type
  */
@@ -146,12 +147,13 @@ struct SyncTraits<ofdmframesync> {
     };
 
     /**
-     * @brief Wrapper function to get CFR of the last frame received by the OFDM frame synchronizer
+     * @brief Wrapper function to get samples of the last frame received by the OFDM frame synchronizer.
+     * Here the CFR (Channel Frequency Response is used insted of time-domain samples) (no difference in correlation matrix). 
      * 
      * @param fs Pointer to the synchronizer
-     * @param X Vector to store the CFR (Channel Frequency Response) on
+     * @param X Vector to store the samples on
      */
-    static void GetCfr(SynchronizerType fs, std::vector<std::complex<float>>* X) 
+    static void GetFrameSamps(SynchronizerType fs, std::vector<std::complex<float>>* X) 
     {
         unsigned int fft_size = ofdmframesync_get_fft_size(fs);
         X->resize(fft_size);
@@ -164,5 +166,102 @@ struct SyncTraits<ofdmframesync> {
  * 
  */
 using ofdmframesync_iface = SyncTraits<ofdmframesync>;
+
+// -------------------- Flexible Single-Carrier Frame Synchronizer --------------------
+
+/**
+ * @brief SyncTraits specialization for the flexible single-carrier frame synchronizer flexframesync.
+ * Define the functions (Create, Reset, Execute, Destroy, GetFrameSamps) for the flexible frame synchronizer
+ * 
+ * @tparam  Synchronizer type
+ */
+
+template<>
+struct SyncTraits<flexframesync> {
+
+    /**
+     * @brief Define the type of the synchronizer
+     * 
+     */
+    using SynchronizerType = flexframesync;
+
+    /**
+     * @brief Define the type of the callback function used in the frame synchronizer
+     * 
+     */
+    using CallbackType = framesync_callback;
+
+    /**
+     * @brief Define the Parameters for the frame synchronizer Create function
+     * 
+     */
+    struct CreateParams {
+
+    };
+
+    /**
+     * @brief Wrapper function to create a frame synchronizer
+     * 
+     * @param params Synchronizer parameters
+     * @param callback Callback function
+     * @param userdata User-defined data structure
+     * @return SynchronizerType Created synchronizer instance
+     */
+    static SynchronizerType Create(const CreateParams& params, CallbackType callback, void* userdata) 
+    {
+        return flexframesync_create(callback, userdata);
+    };
+
+    /**
+     * @brief Wrapper function to reset a frame synchronizer
+     * 
+     * @param fs 
+     */
+    static void Reset(SynchronizerType fs) 
+    {
+        flexframesync_reset(fs);
+    };
+
+    /**
+     * @brief Wrapper function to execute a frame synchronizer
+     * 
+     * @param fs Pointer to the synchronizer
+     * @param x Pointer to the input samples array
+     * @param n Number of input samples to read
+     * @return int Result of the execution
+     */
+    static int Execute(SynchronizerType fs, std::complex<float>* x, unsigned int n) 
+    {
+        return flexframesync_execute(fs, x, n);
+    };
+
+    /**
+     * @brief Wrapper function to destroy a frame synchronizer
+     * 
+     * @param fs Pointer to the synchronizer
+     */
+    static void Destroy(SynchronizerType fs) 
+    {
+        flexframesync_destroy(fs);
+    };
+
+    /**
+     * @brief Wrapper function to get samples of the last frame received by the frame synchronizer
+     * 
+     * @param fs Pointer to the synchronizer
+     * @param X Vector to store the samples on
+     */
+    static void GetFrameSamps(SynchronizerType fs, std::vector<std::complex<float>>* X) 
+    {
+        // X->resize(fft_size);
+        // flexframesync_get_frame_samps(fs, X->data(), fft_size); // Currently not implemented for flexframesync (t.b.d.) !!!
+    };
+};
+
+/**
+ * @brief Define typename for flexible frame synchronizer interface
+ * 
+ */
+using flexframesync_iface = SyncTraits<flexframesync>;
 
 #endif // SYNCTRAITS_H
