ee5c8dde5d908af382018218e4c4fa56762ac723
F-L-X-S <felixschuelke@icloud.com>
2026-01-16 22:32:12 +0100

    Test for music with gnuradio

---
+++ simulations/music_gnuradio/music_gnuradio.cc
@@ -0,0 +1,81 @@
+/**
+ * @file music_gnuradio.cc
+ * @author Felix Schuelke (flxscode@gmail.com)
+ * 
+ * @brief 
+ * @version 0.1
+ * @date 2026-01-16
+ * 
+ */
+
+ #include <iostream>
+ #include <csignal>
+
+ #include <liquid.h>
+ #include <sync_worker/sync_worker.h>
+ #include <sdr_interfaces/zmq/interface_zmq.h>
+
+// ZMQ-socket for data import from Gnuradio
+#define IMPORT_INTERFACE "tcp://*:5554" 
+#define NUM_CHANNELS 2
+
+// ZMQ-socket for data export to MUSIC running in Python-application
+#define EXPORT_INTERFACE "tcp://*:5555" 
+
+// Python-Application with MUSIC algorithm for DoA estimation
+#define PYTHONPATH "./music/env/bin/python"
+#define MUSIC_PYFILE "./music/music-spectrum.py"               
+
+// Signal handler to stop by keaboard interrupt
+std::atomic<bool> stop_signal_called(false);
+void sig_int_handler(int) {
+    stop_signal_called=true;
+}
+
+// main function
+int main(int argc, char*argv[])
+{
+    std::signal(SIGINT, &sig_int_handler);
+
+    // Run spectral MUSIC DoA algorithm
+    std::string cmd = std::string(PYTHONPATH) + ' ' + std::string(MUSIC_PYFILE)+"&";
+    system(cmd.c_str());
+
+    // ---------------------- OFDM-Framesync Synchronization Worker ----------------------
+    unsigned int M           = 256;         // number of subcarriers 
+    unsigned int cp_len      = 20;          // cyclic prefix length 
+    unsigned int taper_len   = 4;           // window taper length 
+    unsigned char p[M];                     // subcarrier allocation array
+    ofdmframe_init_default_sctype(M, p);    // initialize subcarrier allocation
+    SyncWorker<2, ofdmframesync_iface> sync({M, cp_len, taper_len, p}, std::ref(stop_signal_called));
+
+    // Add output-queues to sync-worker 
+    // FrameSampsQueue_t frame_samps_queue;
+    // sync.AddFrameSampsQueue(std::ref(frame_samps_queue));
+    // FrameSymsQueue_t frame_syms_queue;
+    // sync.AddFrameSymsQueue(std::ref(frame_syms_queue));
+
+    // ---------------------- ZMQ Worker ----------------------
+    // ZMQ-socket for data import from Gnuradio
+    auto& rx_queues = *sync.GetRxQueues();
+    ZmqRxWorker<NUM_CHANNELS> zmq_rx_worker(IMPORT_INTERFACE, rx_queues, stop_signal_called);
+
+    // ZMQ socket for data export to MUSIC Python-application
+    MultiChFrameSampsQueue_t tx_queue;
+    ZmqTxWorker<MultiChFrameSamps_t> zmq_tx_worker(EXPORT_INTERFACE, tx_queue, stop_signal_called);
+
+    // ---------------------- Run Workers ----------------------
+    sync.RunWorker();   
+    zmq_rx_worker.RunWorker();
+    zmq_tx_worker.RunWorker();
+
+    while (!stop_signal_called.load()) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+
+    sync.StopWorker();
+    zmq_rx_worker.StopWorker();
+    zmq_tx_worker.StopWorker();
+
+    return 0;
+}
