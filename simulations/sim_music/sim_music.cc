0a992bf798584d84712f302d2c52be224602a34d
F-L-X-S <felixschuelke@icloud.com>
2026-01-19 13:01:05 +0100

    zmq_if as cmake subproject

--- simulations/sim_music/sim_music.cc
+++ simulations/sim_music/sim_music.cc
@@ -74,7 +74,7 @@
  #include <signal_generator/signal_generator.h>
  #include <matlab_export/matlab_export.h>
  #include <multisync/multisync.h>
- #include <interfaces/zmq/zmq_if.h>
+ #include <zmq_if.h>
 
 // Definition of the transmission-settings 
 #define NUM_SAMPLES 1200            // Total Number of samples to be generated 
