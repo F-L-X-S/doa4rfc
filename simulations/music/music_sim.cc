a8c68bcf5be4ded74995bddbe5562f483c9924d1
F-L-X-S <felixschuelke@icloud.com>
2026-03-25 19:33:36 +0100

    reduced cycle time in main thread

--- simulations/music/music_sim.cc
+++ simulations/music/music_sim.cc
@@ -294,7 +294,7 @@ int main(int argc, char*argv[])
     
     // Wait until program is exited by user... 
     while (!stop_signal_called.load()) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
     }
 
     terminal.StopWorker();
